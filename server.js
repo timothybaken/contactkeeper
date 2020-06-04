6da676f0793638c47b6956d5d74fbf8f5eb4d9a3
Timothy Baken <timothy.baken@gmail.com>
2020-06-04 19:09:17 +0200

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -17,6 +18,19 @@ app.get("/", (req, res) =>
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get(
+    "*",
+    (req, res) =>
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    // sends the index.html file inside directory/client/build/index.html
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
